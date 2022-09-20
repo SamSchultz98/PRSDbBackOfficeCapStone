da107983f2cf97bd33cc78aecb48376cb17f05a3
Sam Schultz <sam.schultz@outlook.com>
2022-09-20 11:14:11 -0400

    AddedVendorClassANDVendorController

--- PRSDbBackOfficeCapStone/Migrations/20220920145844_AddedVendor.Designer.cs
+++ PRSDbBackOfficeCapStone/Migrations/20220920145844_AddedVendor.Designer.cs
@@ -2,6 +2,7 @@
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using PRSDbBackOfficeCapStone.Models;
 
@@ -10,9 +11,10 @@ using PRSDbBackOfficeCapStone.Models;
 namespace PRSDbBackOfficeCapStone.Migrations
 {
     [DbContext(typeof(AppDbContext))]
-    partial class AppDbContextModelSnapshot : ModelSnapshot
+    [Migration("20220920145844_AddedVendor")]
+    partial class AddedVendor
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
--- PRSDbBackOfficeCapStone/Models/AppDbContext.cs
+++ PRSDbBackOfficeCapStone/Models/AppDbContext.cs
@@ -6,14 +6,16 @@ namespace PRSDbBackOfficeCapStone.Models
     public class AppDbContext : DbContext
     {
 
-
+        //Database Tables
         public DbSet<User> Users { get; set; }
+        public DbSet<Vendor> Vendors { get; set; }
+
+
+
+
 
         public AppDbContext(DbContextOptions<AppDbContext> Options) : base(Options) { }
-
-
         protected override void OnModelCreating(ModelBuilder builder) { }
-
 
     }
 }
