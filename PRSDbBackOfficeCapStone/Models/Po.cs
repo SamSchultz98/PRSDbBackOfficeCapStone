ecc7c8d945b150c7b0e502693509e78b4d97b8e1
Sam Schultz <sam.schultz@outlook.com>
2022-09-22 09:18:56 -0400

    Made the Po and POLine class

---
+++ PRSDbBackOfficeCapStone/Models/Po.cs
@@ -0,0 +1,16 @@
+﻿namespace PRSDbBackOfficeCapStone.Models
+{
+    public class Po
+    {
+        public Vendor Vendor { get; set; }
+        public IEnumerable<Poline> polines { get; set; }
+        public decimal PoTotal { get; set; }
+    }
+    public class Poline
+    {
+        public string Product { get; set; }
+        public int Quantity { get; set; }
+        public decimal Price { get; set; }
+        public decimal LineTotal { get; set; }
+    }
+}
